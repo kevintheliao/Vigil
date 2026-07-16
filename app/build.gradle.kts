d01e1df042e48e06c62fe27befb4ed431597ae7a
Kevin Liao <kevin.liao.cheetah@gmail.com>
2026-07-15 21:36:25 -0700

    Add ONNX Runtime Android dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.27.0")
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
